4931b007ca5149c222127e1aa19419c658598a04
Arturo Cannarozzo Techfolla <arturo.cannarozzo@pm.me>
2026-04-08 10:58:57 +0200

    Update Order_ingestion.py

--- Order_ingestion.py
+++ Order_ingestion.py
@@ -1,2 +1,14 @@
-#! /usr/bin/python3
-# 
+#!/usr/bin/python3
+
+# Import packages
+import os
+import pandas as pd
+import shutil 
+from datetime import datetime
+
+# Define folders and paths 
+data_folder = "data"
+archive_folder = os.path.join(data_folder, "archive")
+insights_folder = "insights"
+logs_folder = "logs"
+log_path = os.path.join(logs_folder
